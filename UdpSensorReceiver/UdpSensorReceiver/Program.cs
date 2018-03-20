1b0bfb431082ab85f506cc86083df094d0bc100a
IanMFrost <ianmfrost0@gmail.com>
2018-03-20 11:19:56 +0100

    Splitter beskeden fra broadcasteren

--- UdpSensorReceiver/UdpSensorReceiver/Program.cs
+++ UdpSensorReceiver/UdpSensorReceiver/Program.cs
@@ -13,22 +13,47 @@ namespace UdpSensorReceiver
         static void Main(string[] args)
         {
 
-            UdpClient udpSensor = new UdpClient(6789);
+            UdpClient udpSensor = new UdpClient(7000);
             
 
             //Creates an IPEndPoint to record the IP Address and port number of the sender.  
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any,6789);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any,3333);
+
+
 
             while (true)
             {
                 Byte[] datagramRecieved = udpSensor.Receive(ref RemoteIpEndPoint);
                 string message = Encoding.ASCII.GetString(datagramRecieved,0 , datagramRecieved.Length);
 
-                Console.WriteLine(message);
+                string[] formattetstrings = message.Split('\n');
 
-            
-                Console.WriteLine(message);
+                string CO = formattetstrings[3];
+                string NOX = formattetstrings[4];
+
+                string[] coString = CO.Split(':');
+                string[] noxString = NOX.Split(':');
+
+                int noxValue;
+                double coValue;
+
+                Int32.TryParse(noxString[1], out noxValue);
+                double.TryParse(coString[1], out coValue);
+
+                Console.WriteLine("current CO: " + coValue);
+                Console.WriteLine("Current NOX: " + noxValue);
+                
+
+                //foreach (var line in formattetstrings)
+                //{
+                //    Console.WriteLine(line);
+                //    Console.WriteLine();
+                //}
+
+
+
             }
+
         }
     }
 }
