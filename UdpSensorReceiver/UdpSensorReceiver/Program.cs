54d778d5573408fe3d85f7dbfa516c4e0db4caf7
IanMFrost <ianmfrost0@gmail.com>
2018-03-20 10:31:46 +0100

    Oprettet udpsensorreciever

---
+++ UdpSensorReceiver/UdpSensorReceiver/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpSensorReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UdpClient udpSensor = new UdpClient(6789);
+            
+
+            //Creates an IPEndPoint to record the IP Address and port number of the sender.  
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any,6789);
+
+            while (true)
+            {
+                Byte[] datagramRecieved = udpSensor.Receive(ref RemoteIpEndPoint);
+                string message = Encoding.ASCII.GetString(datagramRecieved,0 , datagramRecieved.Length);
+
+                Console.WriteLine(message);
+
+            
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
